82a990515e1df64dfb9c6f6fc0559edafa16f791
Michail Karatzidis <mkaratzi@c1r2p3.hive.fi>
2022-08-31 13:41:24 +0300

    Fill_square and print_map cleaned and norminetted

--- src/map.c
+++ src/map.c
@@ -3,23 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mkaratzi <mkaratzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 13:26:52 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/08/31 13:30:47 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/08/31 13:40:38 by mkaratzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
-//	Fills the given square into the map.
+/*	Fills the given square into the map. */
 void	fill_square(t_map *map, t_square *square)
 {
-	
+	int	k;
+	int	y;
+
+	y = solved_square->y;
+	while (y < (solved_square->y + solved_square->size)
+		&& y <= map->header->height)
+	{
+		k = solved_square->x - solved_square->size + 1;
+		while (k <= solved_square->x)
+		{
+			map->map[y][k] = map->header->filler;
+			k++;
+		}
+	y++;
+	}
+	print_map(map);
 }
 
-//	Prints the map.
+/*	Prints the map. */
 void	print_map(t_map *map)
 {
-	
+	int	y;
+
+	y = 0;
+	while (y <= map->header->height)
+	{
+		write(1, map->map[y], map->header->width);
+		write(1, "\n", 1);
+		y++;
+	}
 }
